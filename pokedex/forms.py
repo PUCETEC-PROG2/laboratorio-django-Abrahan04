49aec92e459f5a1b9824495fa23fe9ba8d1aab91
Abrahan Masabanda <jamasabanda@puce.edu.ec>
2025-01-22 23:10:14 -0500

    Taller  10 completo con migraciones

--- pokedex/forms.py
+++ pokedex/forms.py
@@ -1,23 +1,46 @@
 from django import forms
-from .models import Pokemon
+from .models import Pokemon, Trainer
+
+##
+class TrainerForm(forms.ModelForm):
+    class Meta:
+        model = Trainer
+        fields = '__all__'
+        labels = {
+            'first_name': 'Nombre',
+            'last_name' : 'Apellido',
+            'birth_date': 'Nacimiento',
+            'level': 'nivel',
+            'picture': 'Foto'
+        }
+        widgets = {
+            'first_name' : forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name' : forms.TextInput(attrs={'class': 'form-control'}),
+            'birth_date' : forms.DateInput(attrs={'class': 'form-control'}),
+            'level' : forms.NumberInput(attrs={'class': 'form-control'}),
+            'picture' : forms.ClearableFileInput(attrs={'class': 'form-control'})
+        }
+
+
 
 class PokemonForm(forms.ModelForm):
     class Meta:
-        model = Pokemon 
+        model = Pokemon
         fields = '__all__'
         labels = {
             'name': 'Nombre',
-            'type': 'Tipo',
+            'type' : 'Tipo',
             'height': 'Altura',
             'weight': 'Peso',
             'trainer': 'Entrenador',
             'picture': 'Foto'
         }
-        widgets ={
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'type': forms.Select(attrs={'class': 'form-control'}),
-            'height': forms.NumberInput(attrs={'class': 'form-control'}),
-            'weight': forms.NumberInput(attrs={'class': 'form-control'}),
-            'trainer': forms.Select(attrs={'class': 'form-control'}),
-            'picture': forms.ClearableFileInput(attrs={'class': 'form-control'})
+        widgets = {
+            'name' : forms.TextInput(attrs={'class': 'form-control'}),
+            'type' : forms.Select(attrs={'class': 'form-control'}),
+            'height' : forms.NumberInput(attrs={'class': 'form-control'}),
+            'weight' : forms.NumberInput(attrs={'class': 'form-control'}),
+            'trainer' : forms.Select(attrs={'class': 'form-control'}),
+            'picture' : forms.ClearableFileInput(attrs={'class': 'form-control'})
         }
+        
--- pokedex/urls.py
+++ pokedex/urls.py
@@ -5,11 +5,16 @@ from . import views
 app_name = 'pokedex'
 
 urlpatterns = [
-    path("", views.index, name="index"), #Pantalla de Pokemons
+    path("", views.index, name="index"), 
     path("<int:pokemon_id>/", views.pokemon, name="pokemon"),
-    path("trainer/<int:trainer_id>/", views.trainer_detail, name="trainer"),
-    path("<add_pokemon/", views.add_pokemon, name="add_pokemon"),
-    path("edit_pokemon/<int:pokemon_id>/", views.edit_pokemon, name="edit_pokemon"), 
-    path("delete_pokemon/<int:pokemon_id>/", views.delete_pokemon, name="delete_pokemon"), 
+    path("trainer/<int:trainer_id>/", views.trainer, name="trainer"),
+    path("info_pokemons/", views.info_pokemons, name="info_pokemons"),
+    path("info_trainers/", views.info_trainers, name="info_trainers"),
+    path("add_trainer/", views.add_trainer, name="add_trainer"),
+    path("edit_trainer/<int:trainer_id>/", views.edit_trainer, name="edit_trainer"),
+    path("delete_trainer/<int:trainer_id>/", views.delete_trainer, name="delete_trainer"),
+    path("add_pokemon/", views.add_pokemon, name="add_pokemon"),
+    path("edit_pokemon/<int:pokemon_id>/", views.edit_pokemon, name="edit_pokemon"),
+    path("delete_pokemon/<int:pokemon_id>/", views.delete_pokemon, name="delete_pokemon"),
     path("login/", views.CustomLoginView.as_view(), name="login")
 ]
